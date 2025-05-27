a46f858da63477320bfc2fb518c5a76c2c1de9be
Ton Nom <ton.email@example.com>
2025-05-27 02:04:08 +0100

    Initial commit - Sauvegarde de PaxBank

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("login-btn").addEventListener("click", function() {
+    alert("🔐 Connexion en cours...");
+    // Ici, tu peux ajouter une redirection ou l'ouverture d'un formulaire de connexion.
+});
