42a938041a8e8e083daeb0f73c13c3142bd13816
Ton Nom <ton.email@example.com>
2025-05-28 20:30:08 +0100

    Correction du double import de express et optimisation du serveur

--- server.mjs
+++ server.mjs
@@ -1,16 +1,12 @@
-
 const express = require('express');
 const path = require('path');
 const app = express();
 
-// Servir les fichiers statiques du dossier "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Pour toute requête à la racine, envoyer `index.html`
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Définir le port utilisé
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`✅ PaxBank est opérationnel sur le port ${port}`));
